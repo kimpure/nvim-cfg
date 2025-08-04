57118eb14c443abc8f798a402ff8a0ccdde742a1
kimpure <gimleni593@gmail.com>
2025-08-04 22:51:02 +0900

    Change filetree

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,8 +1,6 @@
 local require_dir = require("utils/require-dir")
 
 require("config.lazy")
-
-require("config.globals")
 require("config.keymaps")
-require("config.options")
+require_dir("config.vim")
 require_dir("config.style")
---
+++ lua/config/vim/options.lua
@@ -0,0 +1,25 @@
+vim.opt.shadafile = "NONE"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.encoding = "UTF-8"
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 10
+vim.opt.mouse:append("a")
+
+vim.opt.laststatus = 2
+
+vim.o.updatetime = 500
+vim.o.showmatch = false
