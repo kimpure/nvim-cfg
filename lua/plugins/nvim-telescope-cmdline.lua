44636f5fb17d158edf0e716ca53399eab623e57c
kimpure <gimleni593@gmail.com>
2025-08-04 22:19:34 +0900

    Change layout size

--- lua/plugins/nvim-telescope-cmdline.lua
+++ lua/plugins/nvim-telescope-cmdline.lua
@@ -4,20 +4,19 @@ return {
 		require("telescope").setup({
 			extensions = {
 				cmdline = {
-					-- Adjust telescope picker size and layout
 					picker = {
 						layout_config = {
-							width = 120,
+							width = 90,
 							height = 25,
 						},
 					},
-					-- Adjust your mappings
+
 					mappings = {
 						complete = "<Tab>",
 						run_selection = "<C-CR>",
 						run_input = "<CR>",
 					},
-					-- Triggers any shell command using overseer.nvim (`:!`)
+
 					overseer = {
 						enabled = true,
 					},
