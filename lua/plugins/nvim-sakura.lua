3fde00d619e12fad44ba3d1fb97c6d5188f9c129
kimpure <gimleni593@gmail.com>
2025-08-30 20:38:04 +0900

    fix setting

--- lua/plugins/nvim-sakura.lua
+++ lua/plugins/nvim-sakura.lua
@@ -2,6 +2,7 @@ return {
 	"anAcc22/sakura.nvim",
 	priority = 1000, -- Ensure it loads first
 	config = function()
-		vim.cmd("colorscheme onedark")
+		vim.opt.background = "dark" -- or "light"
+		vim.cmd("colorscheme sakura") -- sets the colorscheme
 	end,
 }
