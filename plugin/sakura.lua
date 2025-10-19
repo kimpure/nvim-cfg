422d78b0a79ce16e7a06d15837f078fe8e576c17
kimpure <gimleni593@gmail.com>
2025-10-19 20:25:06 +0900

    feat: Add theme

---
+++ plugin/sakura.lua
@@ -0,0 +1,28 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function()
+		local colors = {
+			add = "#81b88b",
+			change = "#e2c08d",
+			delete = "#c74e39",
+            ignore = "#9d9d9d",
+		}
+
+		vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = colors.add, bold = false, cterm = nil })
+		vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = colors.change, bold = false, cterm = nil })
+		vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = colors.delete, bold = false, cterm = nil })
+        vim.api.nvim_set_hl(0, "NvimTreeGitIgnored", { fg = colors.ignore })
+
+        vim.api.nvim_set_hl(0, "Directory", { bold = false })
+    end,
+})
+
+pack.add({
+	{ src = "https://github.com/rktjmp/lush.nvim" },
+	{
+		src = "https://github.com/kimpure/sakura.nvim",
+		boot = function()
+			vim.cmd("colorscheme sakura")
+		end,
+	},
+})
+
