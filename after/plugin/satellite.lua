2d7652f02cf32c5b57b978a4f57b424dd1f06822
kimpure <gimleni593@gmail.com>
2025-10-20 16:43:29 +0900

    style: Format code

--- after/plugin/satellite.lua
+++ after/plugin/satellite.lua
@@ -3,9 +3,9 @@ local satellite = require("satellite")
 satellite.setup({
 	current_only = false,
 	winblend = 50,
-    handlers = {
-        marks = {
-            enable = false,
-        },
-    }
+	handlers = {
+		marks = {
+			enable = false,
+		},
+	},
 })
