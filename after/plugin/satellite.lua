e0dc07adf4e28910cc56c49d88502482e1c80a68
kimpure <gimleni593@gmail.com>
2025-10-20 15:56:31 +0900

    feat: Add statellite.nvim

---
+++ after/plugin/satellite.lua
@@ -0,0 +1,11 @@
+local satellite = require("satellite")
+
+satellite.setup({
+	current_only = false,
+	winblend = 50,
+    handlers = {
+        marks = {
+            enable = false,
+        },
+    }
+})
---
+++ plugin/satellite.lua
@@ -0,0 +1,3 @@
+pack.add({
+	{ src = "https://github.com/lewis6991/satellite.nvim" },
+})
