2eb5f6bf8608dbd9b4f60c8a9ef85eac9f05fdd5
kimpure <gimleni593@gmail.com>
2025-10-22 16:21:02 +0900

    feat: Remove smart-split.nvim

--- after/plugin/smart-splits.lua
+++
@@ -1,7 +0,0 @@
-local smart_splits = require("smart-splits")
-local keymap = vim.keymap
-
-keymap.set("n", "<C-left>", smart_splits.resize_left)
-keymap.set("n", "<C-down>", smart_splits.resize_down)
-keymap.set("n", "<C-up>", smart_splits.resize_up)
-keymap.set("n", "<C-right>", smart_splits.resize_right)
--- plugin/smart-splits.lua
+++
@@ -1,3 +0,0 @@
-pack.add({
-	{ src = "https://github.com/mrjones2014/smart-splits.nvim" },
-})
