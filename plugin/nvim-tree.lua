f8ed6ea5c6c281192ffe92b72f8ccfeb06eff07d
kimpure <gimleni593@gmail.com>
2025-10-25 11:32:24 +0900

    feat: Add nvim-material-icon

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,5 +1,5 @@
 pack.add({
 	{ src = "github.com/nvim-lua/plenary.nvim" },
-	{ src = "github.com/nvim-tree/nvim-web-devicons" },
+	-- { src = "github.com/nvim-tree/nvim-web-devicons" },
 	{ src = "github.com/nvim-tree/nvim-tree.lua" },
 })
---
+++ plugin/theme/nvim-material-icon.lua
@@ -0,0 +1,8 @@
+pack.add({
+    {
+        src = "github.com/DaikyXendo/nvim-material-icon",
+	    boot = {
+            "nvim-web-devicons",
+        },
+    },
+})
